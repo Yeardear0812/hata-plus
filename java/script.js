980ab966ffdc6f39a6ac6c296b92defc43c2bb4b
Yeardear0812 <yeardear18@gmail.com>
2026-01-27 21:10:04 +0200

    fixandadaptiv

--- java/script.js
+++ java/script.js
@@ -13,3 +13,39 @@ cancel.onclick = (e) => {
     mainnav.style.left = '-100%';
     black1.style.display = 'none';
 };
+
+function openMenu(id) {
+    id.style.display = 'block'
+    id.style.position = 'fixed'
+    id.style.top = '0'
+    id.style.left = '0'
+    id.style.width = '100%'
+    id.style.zIndex = '1'
+}
+  
+function closeMenu(id) {
+    id.style.display = 'none'
+    id.style.position = 'static'
+}
+if(window.innerWidth < 500) {
+    btn.onclick = (e) => {
+    e.preventDefault()
+    openMenu(submenu)
+}
+  
+subbtn.onclick = (e) => {
+    e.preventDefault()
+    openMenu(subsubmenu)
+}
+  
+cancelsub.onclick = (e) => {
+    e.preventDefault()
+    closeMenu(submenu)
+}
+cancelsubsub.onclick = (e) => {
+    e.preventDefault()
+    closeMenu(subsubmenu)
+}
+}
+  
+  
